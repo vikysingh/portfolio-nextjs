e8edb27c96d14f6cc14059e8fe2ef9f084809d5c
Vikram Singh <singhvik071@gmail.com>
2020-12-17 13:35:43 +0100

    Iniziato lavorare su about page

---
+++ pages/about.js
@@ -0,0 +1,34 @@
+import { Container, Col, Row } from "react-grid-system"
+import { HiMenuAlt4, HiArrowNarrowDown } from "react-icons/hi"
+import { Tipografia, Tasto } from "../Componenti/Libreria"
+
+const about = () => {
+    return (
+        <Container>
+            <Row>
+                <HiMenuAlt4 />
+            </Row>
+            <Row>
+                <Row>
+                    <Col>
+                        <Tipografia livello="h2" variato bold colore="rosso" > Vikram Singh </Tipografia>
+                        <Tipografia livello="paragrafo"  >
+                            Testo descrittivo va scritto qui, probabilmente meglio se abbastanza lungo.
+                            Oltre a questo anche una lunga ma lunga proprio lunga descrizione di about,
+                            oh yeah!
+                        </Tipografia>
+                    </Col>
+                    <Col>
+                        <img src="https://vikramsingh.netlify.app/src/assets/profile.jpeg"
+                        alt="Foto profilo" />
+                    </Col>
+                </Row>
+                <Row>
+                    <Tasto> <Tipografia livello="h4" bold >Scarica CV <HiArrowNarrowDown /></Tipografia> </Tasto>
+                </Row>
+            </Row>
+        </Container>
+    );
+}
+
+export default about;
