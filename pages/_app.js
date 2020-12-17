a92b34713217b07fcfba7b16e665e7481acc41aa
Vikram Singh <singhvik071@gmail.com>
2020-12-17 09:29:53 +0100

    Aggiunto sistema griglia, react-grid-system

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,35 @@
-import '../styles/globals.css'
+import { createGlobalStyle } from "styled-components"
+import Head from "next/head"
+
+const GlobalStyle = createGlobalStyle`
+*, body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Quicksand', sans-serif;
+    color: #1C1D25;
+}
+*:focus, body:focus {
+    outline: none;
+}
+`
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+  <Head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="theme-color" content="#55E6C1" />
+      <meta name="description" content="Sviluppatore FrontEnd" />
+
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&family=Quicksand&display=swap" rel="stylesheet" />
+
+      <title>Vikram Singh</title>
+  </Head>
+  <GlobalStyle />
+  <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,26 @@
-import { TextField } from "../Componenti/Libreria"
+import { Tipografia } from "../Componenti/Libreria"
+import { Container, Row, Col } from "react-grid-system"
 
-const { TextArea } = TextField
 
 export default function Home() {
   return (
-    <TextArea placeholder="ciao" />
+    <Container>
+      <Row>
+        <Tipografia livello="paragrafo" >Row singolo</Tipografia>
+      </Row>
+      <Row>
+        <Col>
+          <Tipografia livello="h1" variato bold colore="rosso">Row 2 col1</Tipografia>
+          <Tipografia livello="paragrafo" >Un po' di descrizione</Tipografia>
+
+          <Row>
+            Row 2 Row 3
+          </Row>
+        </Col>
+        <Col>
+          Row 2 col 3
+        </Col>
+      </Row>
+    </Container>
   )
 }
