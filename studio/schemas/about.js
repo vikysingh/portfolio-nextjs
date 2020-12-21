a947524395801facb6d4d28329adef3a1f7715e9
Vikram Singh <singhvik071@gmail.com>
2020-12-21 16:09:44 +0100

    Impostazione iniziale

---
+++ studio/schemas/about.js
@@ -0,0 +1,37 @@
+export default {
+    title: 'About',
+    name: 'about',
+    type: 'document',
+    fields: [
+        {
+            title: 'Titolo',
+            name: 'titolo',
+            type: 'string'
+        },
+        {
+            title: 'Descrizione',
+            name: 'descrizione',
+            type: 'string'
+        },
+        {
+            title: 'CVLink',
+            name: 'cvlink',
+            type: 'string'
+        },
+        {
+            title: 'CVCTA',
+            name: 'cvcta',
+            type: 'string'
+        },
+        {
+            title: 'ImgSrc',
+            name: 'imgsrc',
+            type: 'string'
+        },
+        {
+            title: 'ImgAlt',
+            name: 'imgalt',
+            type: 'string'
+        }
+    ]
+}
---
+++ studio/schemas/contatti.js
@@ -0,0 +1,32 @@
+export default {
+    title: 'Contatti',
+    name: 'contatti',
+    type: 'document',
+    fields: [
+        {
+            title: 'Titolo primario',
+            name: 'titoloprimario',
+            type: 'string'
+        },
+        {
+            title: 'Titolo secondario',
+            name: 'titolosecondario',
+            type: 'string'
+        },
+        {
+            title: 'Descrizione',
+            name: 'descrizione',
+            type: 'string'
+        },
+        {
+            title: 'Email',
+            name: 'email',
+            type: 'string'
+        },
+        {
+            title: 'Telefono',
+            name: 'telefono',
+            type: 'string'
+        }
+    ]
+}
