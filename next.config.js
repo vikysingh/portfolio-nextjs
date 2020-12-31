fe05492a12203e12a96fa68d9f14827d59c944a0
Vikram Singh <singhvik071@gmail.com>
2020-12-31 18:19:33 +0100

    aggiunto next-compose-plugins e @next/bundle-analyzer

---
+++ next.config.js
@@ -0,0 +1,8 @@
+const withPlugins = require("next-compose-plugins")
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+    enabled: process.env.ANALYZE === "true",
+})
+
+module.exports = withPlugins([
+    [withBundleAnalyzer],
+])
