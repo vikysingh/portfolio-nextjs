d2043b69999fc4aaaa8fd737723b3e164692c340
Vikram Singh <singhvik071@gmail.com>
2022-01-30 14:40:58 +0100

    add notice page

--- Componenti/Context/StudioContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react"
-
-const StudioContext = createContext()
-
-export default StudioContext
--- client.js
+++
@@ -1,7 +0,0 @@
-import sanityClient from "@sanity/client"
-
-export default sanityClient({
-    projectId: "8ou871uz",
-    dataset: "portfolio",
-    useCdn: false
-})
